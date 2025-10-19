1ece2d8829cf0047982d4dfbe0b4cd93db04e5a4
Asmik <asjestor24@gmail.com>
2025-10-19 20:49:45 +0300

    Циклы

---
+++ циклы.py
@@ -0,0 +1,55 @@
+#1
+# for i in range (0,11):
+#     print(i)
+
+#2
+# for i in range (1,21):
+#     if i %2==0:
+#         continue
+#     print(i)
+
+#2.1
+# for i in range(2, 21, 2):
+#     print(i)
+
+#3
+# total = 0
+# for i in range(1, 101):
+#     total += i
+# print("сумма:", total)
+
+#4
+# for i in range(1, 11):
+#     print(f"5 x {i} = {5 * i}")
+
+#5
+# for i in range(1, 11):
+#     print(f"{i}² = {i ** 2}")
+
+#6
+# for i in range(10, 0, -1):
+#     print(i)
+
+#7
+# for i in range (1,51,2):
+#     print(i)
+#
+
+#8
+# for i in range(1, 31):
+#     if i % 3 == 0:
+#         print("p")
+#     else:
+#         print(i)
+
+#9
+# for i in range(1, 51):
+#     if i % 4 == 0:
+#         continue
+#     print(i)
+
+#10
+# for i in range (1,11):
+#     if i==5:
+#         break
+#     print(i)
